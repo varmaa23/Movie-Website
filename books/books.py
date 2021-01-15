0ae34b9cfb3974705d66188537f543b14f985238
Valentina Guerrero <valentinaguerrero@Valentinas-MacBook-Pro.local>
2021-01-14 20:57:59 -0500

    Improved find years method

--- books/books.py
+++ books/books.py
@@ -12,10 +12,8 @@ with open('books.csv', newline='') as csvfile:
         publicaton_year = row[1]
         author = row[2]
 
-        
-
         regex = "\s\((.*)\)"
-        if(author.find(" and ") == -1):
+        if (author.find(" and ") == -1):
             author = re.sub(regex, "", author)
         else:
             author = author.split(" and ")
@@ -24,14 +22,14 @@ with open('books.csv', newline='') as csvfile:
 
         BooksDataSet.append([title, publicaton_year, author])
 
+
 def convertStringToInt(year):
     if len(year) == 4:
         try:
-             int_year = int(year)
+            int_year = int(year)
+            return int_year
         except:
             print("Please input an integer")
-        return int_year
-    return
 
 
 def find_authors(author_input):
@@ -39,31 +37,39 @@ def find_authors(author_input):
     for book in BooksDataSet:
         author = book[2]
         title = book[0]
-        if (type(author) is str and author.lower().find(author_input.lower()) != -1):
+        if type(author) is str and author.lower().find(author_input.lower()) != -1:
 
             authors_array.append([author, title])
         elif (type(author) is list):
             for authorIndex in range(len(author)):
-                if (author[authorIndex].lower().find(author_input.lower()) != -1):
+                if author[authorIndex].lower().find(author_input.lower()) != -1:
                     authors_array.append([author, title])
     return authors_array
+
 
 def find_titles(title_input):
     print(title_input, type(title_input))
     title_array = []
     for book in BooksDataSet:
         title = book[0]
-        if (type(title) is str and title.lower().find(title_input.lower()) != -1):
+        if type(title) is str and title.lower().find(title_input.lower()) != -1:
             title_array.append(title)
-    return title_array   
+    return title_array
+
 
 def find_years(year1, year2):
+    books_array = []
     year1 = convertStringToInt(year1)
     year2 = convertStringToInt(year2)
-    # for book in BooksDataSet:
-    #     title = book[0]
-    #     if (type(title) is str and title.lower().find(title_input.lower()) != -1):
-    #         title_array.append(title)
+    for book in BooksDataSet:
+        publicaton_year = book[1]
+        if year1 < int(publicaton_year) < year2 or year2 < int(publicaton_year) < year1:
+            books_array.append(book)
+    return books_array
+
+
+def display_books_by_year(books_array):
+    print(books_array, sep='n')
 
 
 def main():
@@ -72,12 +78,11 @@ def main():
     group.add_argument("-a", "--author", action="store_true")
     group.add_argument("-t", "--title", action="store_true")
     group.add_argument("-y", "--year", action="store_true")
-    parser.add_argument("Input", nargs="*", type = str, help="The search string") 
-    # parser.add_argument("Input", type=int, help="The search string", required=False) 
+    parser.add_argument("Input", nargs="*", type=str, help="The search string")
+    # parser.add_argument("Input", type=int, help="The search string", required=False)
     args = parser.parse_args()
-    
+
     first_input = args.Input[0]
-        
 
     if len(args.Input) == 1:
         if args.author:
@@ -90,11 +95,12 @@ def main():
         if args.year:
             second_input = args.Input[1]
             print(f'Year, the input is {first_input, second_input}.')
-            find_years(first_input, second_input)
+            display_books_by_year(find_years(first_input, second_input))
         else:
             print("For two arguments, you must use --year")
     else:
         print("Error")
+
 
 main()
 
