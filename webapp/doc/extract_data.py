6952bb584d06a93cde5df240a9395928492847f9
Valentina Guerrero <valentinaguerrero@Valentinas-MacBook-Pro.local>
2021-02-22 23:05:23 -0500

    Changes to extract_data.py

--- webapp/doc/extract_data.py
+++ webapp/doc/extract_data.py
@@ -3,24 +3,29 @@ import csv
 
 # for movies, 1) add movie to movies_table, 2) set ID of each movie, 
 def get_data(file_name):
-    movies_table = {}
+    movies_table = []
     with open(file_name, mode="r") as file:
         movie_data = csv.reader(file, delimiter=",")
         headers = next(movie_data)
         headers = remove_extra_line_info(headers)
         print(headers)
         for line in movie_data:
-            line = remove_extra_line_info(line)
-            for i in range(len(headers)):
-                movies_table[i] = line
+        	if remove_extra_line_info(line):
+		        line = remove_extra_line_info(line)
+		        movies_table.append(line)
         return movies_table
 
 def remove_extra_line_info(line):
-    count = 0
-    line2 = line[2:4] + line[6:8] + [line[9]] + line[11:18] + [line[20]] + [line[22]]
-    print(line2, count)
-    count += 1
-    return line2
+	count = 0
+	
+	if line[0] != 'False':
+		return False
+	if line[0] == 'False':
+	    line2 = line[2:4] + line[6:8] + [line[9]] + line[11:18] + [line[20]] + [line[22]]
+	    count += 1
+	    return line2
+
+
 
 def make_unique_movie():
     pass
@@ -37,4 +42,5 @@ def main():
     print(movies)
 
 if __name__ == "__main__":
-    main()
+    results = main()
+    print(results)
